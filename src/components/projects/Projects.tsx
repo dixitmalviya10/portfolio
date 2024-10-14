60a16b3900641bc32b51a59d84525e17e3387241
dixitmalviya10 <dixitmalviya10@gmail.com>
2024-10-14 23:46:27 +0530

    added project link

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,3 +1,5 @@
+import { LinkIcon } from "@heroicons/react/24/outline";
+
 const Projects = () => {
   return (
     <section className="px-5">
@@ -9,7 +11,14 @@ const Projects = () => {
           Projects
         </h1>
         <div className="h-80 grid place-content-center border-4 border-dotted">
-          <h1 className="text-4xl">Coming Soon...</h1>
+          <a
+            href="https://moviebase-psi.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="border border-blue-300 p-5 rounded-lg shadow-inner transition-all relative hover:shadow-xl hover:shadow-blue-50">
+            <LinkIcon className="size-8 absolute text-blue-400 -right-2 -top-2" />
+            <h1 className="text-5xl text-blue-400">Moviebase</h1>
+          </a>
         </div>
       </div>
     </section>
