94ce312abd9fe8d00a8a558565742257c662d6d0
dixitmalviya10 <dixitmalviya10@gmail.com>
2025-06-01 11:27:55 +0530

    feat: added new project

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -10,7 +10,7 @@ const Projects = () => {
           </a>
           Projects
         </h1>
-        <div className="h-80 flex justify-center py-4 flex-wrap items-center gap-5 border-4 border-dotted">
+        <div className="min-h-72 flex justify-center py-4 flex-wrap items-center gap-5 border-4 border-dotted">
           <a
             href="https://moviebase-psi.vercel.app/"
             target="_blank"
@@ -34,6 +34,14 @@ const Projects = () => {
             className="border border-blue-300 p-5 rounded-lg shadow-inner transition-all relative hover:shadow-xl hover:shadow-blue-50">
             <LinkIcon className="size-8 absolute text-blue-400 -right-2 -top-2" />
             <h1 className="text-4xl sm:text-5xl text-blue-400">Calculator</h1>
+          </a>
+          <a
+            href="https://sales-board-six.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="border border-blue-300 p-5 rounded-lg shadow-inner transition-all relative hover:shadow-xl hover:shadow-blue-50">
+            <LinkIcon className="size-8 absolute text-blue-400 -right-2 -top-2" />
+            <h1 className="text-4xl sm:text-5xl text-blue-400">SalesBoard</h1>
           </a>
         </div>
       </div>
